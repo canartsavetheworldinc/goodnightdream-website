0a50f8ba702c4c3cdae4286dcd8aa54c80856e5a
tomo <tomo.mouse.9086@gmail.com>
2018-11-21 08:23:46 +0900

    404をかわいく

--- src/vue/NotFound.vue
+++ src/vue/NotFound.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="page-container">
-		NotFound
+		<div class="error-code">
+			404
+		</div>
+		<div class="logo">
+			<a href="/">
+				<img src="../img/logo_tsp.png" />
+			</a>
+		</div>
+		<div>
+			Sorry, Page Not Found
+		</div>
 	</div>
 </template>
 
@@ -21,5 +31,18 @@
 
 <style scoped>
 	.page-container {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	.error-code {
+		font-size: 30px;
+	}
+	.logo {
+	}
+	.logo img {
+		height: 100%;
 	}
 </style>
--- src/vue/Top.vue
+++ src/vue/Top.vue
@@ -4,9 +4,9 @@
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 		</div>
 		<div class="main-links">
-			<MainLink :src="mainLinks.twitter" title="hoge" href="/twitter"></MainLink>
-			<MainLink :src="mainLinks.story" title="hoge" href="/story"></MainLink>
-			<MainLink :src="mainLinks.demo" title="hoge" href="/demo"></MainLink>
+			<MainLink :src="mainLinks.twitter" title="twitter" href="/twitter"></MainLink>
+			<MainLink :src="mainLinks.story" title="story" href="/story"></MainLink>
+			<MainLink :src="mainLinks.demo" title="demo" href="/demo"></MainLink>
 		</div>
 	</div>
 </template>
