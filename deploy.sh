f32f3ab3a5c10be62b5d72b7616b82721cb43b8d
tomo <tomo.mouse.9086@gmail.com>
2018-11-22 07:10:26 +0900

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
-echo "deploy.sh called"
+#!/bin/bash
+set -e
+rm -rf public || exit 0;
+mkdir public
+
+cp -r src/* public
+
+cd public
+
+git config user.email "canartsavetheworldinc@gmail.com"
+git config user.name "canartsavetheworld"
+
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/canartsavetheworldinc/goodnightdream-website.git" master:gh-pages > /dev/null 2>&1
