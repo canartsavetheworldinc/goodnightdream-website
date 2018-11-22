e2ab6f2b5d769005c5f6d7767e171ed47fc5fa30
tomo <tomo.mouse.9086@gmail.com>
2018-11-23 07:15:42 +0900

    add CNAME for ghpages

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ cd build
 git config user.email "canartsavetheworldinc@gmail.com"
 git config user.name "canartsavetheworld"
 
+echo 'goodnightdream.cf' > CNAME
+
 git init
 git add .
 git commit -m "Deploy: ${TRAVIS_JOB_NUMBER}"
-# git push --force "https://${GH_TOKEN}@github.com/canartsavetheworldinc/goodnightdream-website.git" master:gh-pages
-git push --force "https://${GH_TOKEN}@github.com/canartsavetheworldinc/goodnightdream-website.git" gh-pages
+git push --force "https://${GH_TOKEN}@github.com/canartsavetheworldinc/goodnightdream-website.git" master:gh-pages
